aa8e70757aa613d2fb5456be4032f119d4b6f948
Spartan-626 <kwilliamson0232@gmail.com>
2020-03-08 20:29:13 +0000

    Deleted contact controller and recreated it as contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/", to: "homepage#home", as: "root"
   root "about#index"
-  resources :about, :projects, :contact
+  resources :about, :projects, :contacts
 end
