5d8745699988202c3ade045c130f02d6a968e284
Spartan-626 <kwilliamson0232@gmail.com>
2020-04-19 06:03:27 +0000

    Different contact ofrm added from new tutorial. Error - it thinks the fields are empty when trying to send the message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,10 +4,10 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new{params[:contact]}
+    @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = nil
+      flash.now[:notice] = 'Thank you for your message. I will contact you soon!'
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
